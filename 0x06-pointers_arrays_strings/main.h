51aef79704e7919882fe223d1831cf8711d91fab
Isek7 <bengabrielisek@gmail.com>
2023-07-27 12:58:08 -0700

    102-magic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,6 +15,6 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
-
+int main(void);
 
 #endif /* MAIN_H */
